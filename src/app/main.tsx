78d110d9abebb5b57a32f2a86a94d019d0865610
Kajetan Rzepecki <k@spartan.works>
2017-09-11 23:24:23 +0200

    How did this end up in here?

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,7 +2,6 @@ import * as preact from "preact";
 import { MainContainer } from "./containers/main/mainContainer";
 import { mainStore } from "./store/main";
 
-export let hurr = "23";
 export function onLoad() {
   console.log("App successfully loaded!");
   const container = document.createElement("div");
